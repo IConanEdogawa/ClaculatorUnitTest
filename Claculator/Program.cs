b7005358bded9d4165cc83824bc72835742be0da
IConanEdogawa <160710144+IConanEdogawa@users.noreply.github.com>
2024-03-13 22:58:27 +0500

    MathUnit works !

--- Claculator/Program.cs
+++ Claculator/Program.cs
@@ -1,7 +1,17 @@
-﻿internal class Program
+﻿using Claculator;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        // Values -----------------------
+        int a = 5;
+        double b = 4;
+
+        // Result -----------------------
+        var result = MathUnit.Add(a, b);
+
+        // Final write console the result ----------------
+        Console.WriteLine($"This is result => {result}");
     }
 }
