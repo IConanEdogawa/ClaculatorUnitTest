7e7ccebbf6b94bbbf8198108ca71903ca501cf37
IConanEdogawa <160710144+IConanEdogawa@users.noreply.github.com>
2024-03-13 23:05:55 +0500

    First Unit test was wroten

--- TestProject1/CalculatorTest.cs
+++ TestProject1/CalculatorTest.cs
@@ -1,10 +1,22 @@
+using Claculator;
+
 namespace TestProject1
 {
     public class CalculatorTest
     {
         [Fact]
-        public void Test1()
+        public void AddTest()
         {
+            // Arrange  
+            double a = 5;
+            double b = 3;
+            double expected = 8;
+
+            // Act 
+            double actual = MathUnit.Add(a, b);
+
+            // Assert
+            Assert.Equal(expected, actual);
 
         }
     }
